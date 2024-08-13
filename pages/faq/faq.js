8289dafb15bbabb8bc0bff51eb25da7ead85caea
Fonzell <lato954@gmail.com>
2024-08-13 18:01:22 +0300

    Added pages

---
+++ pages/faq/faq.js
@@ -0,0 +1,9 @@
+const accordionButtons = document.querySelectorAll(".accordion button");
+
+for (btn of accordionButtons) btn.addEventListener("click", accordionBtnHandler);
+
+function accordionBtnHandler() {
+  this.nextElementSibling.classList.toggle("expanded");
+  this.querySelector(".vertical-line").classList.toggle("translated");
+  // console.log(this.nextElementSibling.children);
+}
